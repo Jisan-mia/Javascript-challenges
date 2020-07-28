a0a232df5e38a694c4353bc1e891f4e5e25d5d34
Jisan-Mia <jisanmia47@gmail.com>
2020-07-28 21:33:18 +0600

    done with longest word of sent. using real for loop

--- longest word of sent/main.js
+++ longest word of sent/main.js
@@ -2,9 +2,20 @@ function longestWord(str) {
     let words = str.split(" ")
     let longestWord = "";
 
-    for (let word of words) {
-        if (word.length > longestWord.length) {
-            longestWord = word;
+    //using for each loop
+
+    //    for (let word of words) {
+    //        if (word.length > longestWord.length) {
+    //            longestWord = word;
+    //        }
+    //    }
+
+    //Using real for loop
+
+    for (let i = 0; i < words.length; i++) {
+        //console.log(words[i]);
+        if (words[i].length > longestWord.length) {
+            longestWord = words[i];
         }
     }
 
@@ -13,3 +24,4 @@ function longestWord(str) {
 }
 
 console.log(longestWord("We are all friends in the sent."));
+console.log(longestWord("The quick brown fox jumps over the lazy dog"))
